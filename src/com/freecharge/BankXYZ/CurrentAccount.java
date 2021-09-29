7c9033a2e4e3f45ef33ed42c428caf149cbf17e3
FC19507 <91587752+FC19507@users.noreply.github.com>
2021-09-29 16:54:30 +0530

    Update CurrentAccount.java

--- src/com/freecharge/BankXYZ/CurrentAccount.java
+++ src/com/freecharge/BankXYZ/CurrentAccount.java
@@ -1,13 +1,15 @@
 package com.freecharge.BankXYZ;
 
+import java.util.Date;
+
 public class CurrentAccount extends BankDetails {
-	public CurrentAccount() {
-		 super();
+	   String accounttype;
+
+	public CurrentAccount(String name,StringBuilder address,double balance,String status ,Date opendate) {
+		super(name,address,balance,status,opendate);
+		this.accounttype="Current";
 	 }
-	 public void withdraw() {
-		 
-	 }
-	 public void deposite() {
-		 
-	 }
+
+	
+	
 }
