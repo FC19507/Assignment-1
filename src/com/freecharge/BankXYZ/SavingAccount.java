533d583b63e443d8518bda13c976fac524fb96d2
FC19507 <91587752+FC19507@users.noreply.github.com>
2021-09-29 16:55:19 +0530

    Update SavingAccount.java

--- src/com/freecharge/BankXYZ/SavingAccount.java
+++ src/com/freecharge/BankXYZ/SavingAccount.java
@@ -1,14 +1,13 @@
 package com.freecharge.BankXYZ;
 
-public class SavingAccount extends BankDetails{
-	 public SavingAccount() {
-		 super();
-	 }
-	 public void withdraw() {
-		 
-	 }
-	 public void deposite() {
-		 
-	 }
+import java.util.Date;
 
-}
+public class SavingAccount extends BankDetails{
+	   String accounttype;
+
+		public SavingAccount(String name,StringBuilder address,double balance,String status,Date opendate) {
+			super(name,address,balance,status,opendate);
+			this.accounttype="Saving";
+		 }	
+		 
+	 }
