e93cda57eb0110906ec424356374209583d4183c
FC19507 <91587752+FC19507@users.noreply.github.com>
2021-09-29 16:53:40 +0530

    Update BankDetails.java

--- src/com/freecharge/BankXYZ/BankDetails.java
+++ src/com/freecharge/BankXYZ/BankDetails.java
@@ -1,48 +1,82 @@
 package com.freecharge.BankXYZ;
 
+import java.util.Date;
 import java.util.Scanner;
 
 abstract class BankDetails {
-	String name;
-	String addres;
-	double balance;
-	String status;
-	public BankDetails() {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter name,address (city ,state,pin) ,balancen , status( ACTIVE or INACTIVE or CLOSED)");
-		this.name=scanner.nextLine();
-		//scanner.nextLine();
-		this.addres=scanner.nextLine();
-		//scanner.nextLine();
-		this.balance=scanner.nextDouble();
-		this.status=scanner.nextLine();
+	private String name;
+	private StringBuilder address;
+	private double balance;
+	private String status;
+	private Date createdDate;
+	public String getName() {
+		return name;
 	}
-	public void withdraw() {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter Amount that you want to withdraw ");
-		double amount=scanner.nextDouble();
-		if(this.status=="ACTIVE") {
-		if(amount>this.balance) {
-			System.out.println("Balance less then withdraw amount ");
-		}
-		else {
-			this.balance=this.balance-amount;
-			System.out.println("Withdraw amount "+ amount+" succefully done");
-		}
+	public void setName(String name) {
+		this.name = name;
 	}
+	public StringBuilder getAddres() {
+		return address;
+	}
+	public void setAddres(StringBuilder addres) {
+		this.address = addres;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public BankDetails(String name,StringBuilder address,double balance,String status ,Date opendate) {
+		this.name=name;
+		this.address=address;
+		this.balance=balance;
+		this.status=status;
+		this.createdDate=opendate;
+	}
+	 public void withdraw() {
+		 Scanner scanner=new Scanner(System.in);
+			System.out.println("Enter Amount that you want to withdraw ");
+			double amount=scanner.nextDouble();
+			if(this.status=="ACTIVE") {
+			if(amount>this.balance) {
+				System.out.println("Balance less then withdraw amount ");
+			}
+			else {
+				this.balance=this.balance-amount;
+				System.out.println("Withdraw amount "+ amount+" succefully done");
+			}
+		}
+			else
+				System.out.println("Account INACTIVE OR CLOSED");
+	 }
+	 public void deposite() {
+		 Scanner scr=new Scanner(System.in);
+			System.out.println("Enter amount that you want to deposite ");
+			double amount=scr.nextDouble();
+			if(this.status=="ACTIVE") {
+			this.balance+=amount;
+			System.out.println("Successfully Deposite");
+		}
 		else
 			System.out.println("Account INACTIVE OR CLOSED");
+		
+	 }	
+	public void showBalance() {
+		System.out.println("You account balance :"+this.balance);
 	}
-	public void deposite() {
-		Scanner scr=new Scanner(System.in);
-		System.out.println("Enter amount that you want to deposite ");
-		double amount=scr.nextDouble();
-		if(this.status=="ACTIVE") {
-		this.balance+=amount;
-		System.out.println("Successfully Deposite");
-	}
-	else
-		System.out.println("Account INACTIVE OR CLOSED");
-	}	
+	 }
 
-}
+		
